ac8fdb13553a1f0170a9b4ff9873ccbc4d87a779
Harsh Kumar <harshtnm123@gmail.com>
2022-08-13 13:30:29 +0530

    added main content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 !(function () {
+  $(window).resize(function () {
+    AOS.refresh();
+  });
   $(document).ready(function () {
     $(".mobile_nav_show").click(function () {
       $(document.body).toggleClass("mobile_nav_active");
       $(this).toggleClass("bi-x").toggleClass("bi-list");
+    });
+    $(window)
+      .on("scroll", function () {
+        $(this).scrollTop() >= 300
+          ? $(".__main_navbar").addClass("navbar_scroll")
+          : $(".__main_navbar").removeClass("navbar_scroll");
+      })
+      .scroll();
+  });
+  $(window).on("load", function () {
+    AOS.init({
+      duration: 900,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
     });
   });
 })();
