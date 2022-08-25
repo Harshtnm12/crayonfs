c936ffd75ba53752724ed74875a642376c235499
Harsh Kumar <harshtnm123@gmail.com>
2022-08-25 18:34:40 +0530

    added about-us

---
+++ about-us.php
@@ -0,0 +1,72 @@
+<?php include 'header.php'; ?>
+<div class="about_us_page_hero">
+    <div class="container">
+        <div class="about_us_hero_content">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1 class="about_us_hero_title">The digital mortgage</h1>
+                    <h3 class="about_us_hero_sub_title">At Molo, we’re delivering the future of mortgages now!</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<section id="who-we-are">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-3">
+                <h2>Who we are</h2>
+            </div>
+            <div class="col-12 col-md-12 col-lg-9">
+                <p>Molo is an award-winning fintech start-up transforming the mortgage industry as Europe’s only digitally native mortgage lender. Since launching in 2018, we’ve used technology to deliver simpler and faster online mortgage lending to make homeownership easier for everyone.</p>
+                <p>We have led change in the UK mortgage industry, delivering online mortgages to customers with both speed and efficiency. Molo exists solely to make buying a house easier with a digital mortgage designed to let you focus on what really matters. Getting the keys to your new home.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="why-now">
+    <div class="container">
+        <div class="row gy-4 gx-5">
+            <div class="col-12 col-md-12 col-lg-6">
+                <div class="why_now_img_wrapper">
+                    <img src="img/about_focuspicture2.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6">
+                <h2>Why now?</h2>
+                <p>Molo is the UK’s first, fully-digital mortgage lender – that means we are the first player to offer mortgages underwritten fully online. We’re using the power of technology to make real-time lending decisions via a digital, seamless and transparent journey to give customers quicker answers, and a better overall experience.</p>
+                <p>We exist solely to fix what is fundamentally broken in the mortgage market: cumbersome, inefficient, and slow processes, become fast, real-time decisions; a lack of transparency and waiting weeks, only to find out you haven’t been approved, becomes transparency of eligibility and decisioning; and layers of cost and tricksy incentives, becomes fair value, with low fees and no hidden charges.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="What-we-do">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section_head">
+                    <h2>What we do</h2>
+                    <p>
+                        We do mortgages, from residential to buy-to-let. And they’re all entirely online. That means no paperwork or appointments, and you can get a mortgage in a matter of days, rather than weeks. On top of that, we work with intermediaries to expand Molo’s reach and give more people the opportunity to benefit from getting a mortgage completely online.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="our-team">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section_head">
+                    <h2>Our team</h2>
+                    <p>
+                        Molo is an international team, full of smart, dynamic and hands-on people who share the same commitment to digitising mortgages. From tech to marketing and compliance to customer services, we’re building a group of talented individuals who come together to form a highly skilled team full of ambitious people who will change mortgages forever.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="our_team_img"></div>
+</section>
+<?php include 'footer.php'; ?>
